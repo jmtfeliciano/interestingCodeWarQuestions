ce9774f1ff4fd123b8e527b1979148e1159d767e
neonseri <jfeliciano@fas.harvard.edu>
2017-06-24 01:24:51 -0400

    Update and rename RearrangeNumbers.js to LargestRearrangeNumbers.js

--- LargestRearrangeNumbers.js
+++ LargestRearrangeNumbers.js
@@ -11,7 +11,7 @@ function biggest(n){
   array2.sort( (a,b)=>b-a );
 
   if(JSON.stringify(array1) === JSON.stringify(array2) ){
-	  return -1;
+	  return -1; 
   }else{
 	  return Number(array2.join(""));
   }
